a4ae75d398db44c01fa3e7a3c48510a6076e2b09
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-08-30 18:29:40 +0300

    fox5

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -4,12 +4,12 @@ export const ContactListItemBtn = styled.button`
   margin-top: 1rem;
   margin-bottom: 0.5rem;
   align-items: right;
-  background-color: rgba(27, 20, 37, 0.8);
+  background-color: rgba(23, 26, 32, 0.8);
   border: none;
-  border-radius: 8px;
+  border-radius: 7px;
   color: #fff;
   flex-grow: 1;
-  font-family: Arial, sans-serif;
+  font-family: SFProText-Regular, Helvetica, Arial, sans-serif;
   font-size: 15px;
   height: 36px;
   line-height: 20px;
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -4,8 +4,8 @@ export const FilterName = styled.input`
   display: flex;
   flex-direction: column;
   margin: 0.5rem auto 1rem;
-  text-align: left;
   align-items: center;
   font-size: large;
+  text-align: left;
   padding: 0.5rem 15rem 0.5rem 1rem;
 `;
